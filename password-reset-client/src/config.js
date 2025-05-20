5875cab4d56ca8b46c051e88575061489a4ed8b0
Robin-Roy11 <robinroy.ax@gmail.com>
2025-05-20 18:01:45 +0530

    backend final

--- password-reset-client/src/config.js
+++
@@ -1,8 +0,0 @@
-// src/config.js
-
-const API_BASE_URL =
-  import.meta.env.MODE === 'development'
-    ? 'http://localhost:4000'
-    : 'https://task12-4-f8f4.onrender.com';
-
-export default API_BASE_URL;
